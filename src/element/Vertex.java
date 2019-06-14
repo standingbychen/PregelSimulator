98b01811c0005be834ddafe3c66018e71280121d
1161140118 <386506287@qq.com>
2019-06-14 23:15:04 +0800

    update: Vertex

--- src/element/Vertex.java
+++ src/element/Vertex.java
@@ -1,14 +1,29 @@
 package element;
 
-public abstract  class Vertex <VertexValue,EdgeValue,MessageValue> {
-	/**
-	 * 标记顶点活跃状态
-	 * true：参与计算
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ * @author standingby
+ *
+ * @param <V> VatexValue
+ * @param <E> EdgeValue
+ * @param <M> MessageValue
+ */
+public abstract  class Vertex <V,E,M> {
+    public final String vertexId;
+
+    /**
+	 * 标记顶点活跃状态<p>
+	 * true：参与计算<p>
 	 * false：不参与计算，接受消息后转为true
 	 */
 	private boolean active = true;
 	
-	public final String vertexId;	
+	private List<M> lastMessage = new LinkedList<>();
+	private List<M> curMessage = new LinkedList<>();
+	
 
 	
 	public Vertex(String vertexId) {
