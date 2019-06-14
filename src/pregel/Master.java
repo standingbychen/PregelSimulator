19436b139574b667f03f27447b6127d1fa40a94b
1161140118 <386506287@qq.com>
2019-06-15 00:02:45 +0800

    refactor

---
+++ src/pregel/Master.java
@@ -0,0 +1,50 @@
+package pregel;
+
+import java.util.List;
+
+
+public class Master {
+	private List<Worker> works;
+	
+	/**
+	 * 执行运算 SuperStep，启动workers并行计算
+	 */
+	public void run() {
+		
+	}
+	
+	/**
+	 * 导入图
+	 */
+	public void importGraph() {
+		
+	}
+	
+	/**
+	 * 划分图
+	 */
+	public void partition() {
+		
+	}
+	
+	/**
+	 * 保存划分结果
+	 */
+	public void save() {
+		
+	}
+	
+	/**
+	 * 加载划分结果
+	 * @param partitionPath
+	 */
+	public void load(String partitionPath) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/pregel/Worker.java
@@ -0,0 +1,15 @@
+package pregel;
+
+public class Worker extends Thread{
+	
+	
+	/**
+	 * 串行运行各节点的compute函数
+	 */
+	@Override
+	public void run() {
+		super.run();
+	}
+	
+	
+}
