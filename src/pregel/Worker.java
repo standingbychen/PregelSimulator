9d78951159178fca63dcbd063fc724afd7b262d9
1161140118 <386506287@qq.com>
2019-06-15 11:32:50 +0800

    update: worker

--- src/pregel/Worker.java
+++ src/pregel/Worker.java
@@ -1,7 +1,10 @@
 package pregel;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Worker extends Thread{
-	
+	private List<Vertex> vertices = new LinkedList<>();
 	
 	/**
 	 * 串行运行各节点的compute函数
