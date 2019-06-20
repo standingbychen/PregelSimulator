7d9619e2ebb6eb4807f8a09d2bf8b6d816a25cea
1161140118 <386506287@qq.com>
2019-06-20 20:52:45 +0800

    add : comment

--- src/pregel/IUtils.java
+++ src/pregel/IUtils.java
@@ -10,8 +10,13 @@ package pregel;
  */
 public interface IUtils<V, E, M> {
 
-	public Triplet<V, E, M> parseGraphFileLine(String line);
-	
+    /**
+     * 定义输入文件一行内容的解析方式
+     * @param line  一条输入记录
+     * @return  三元组
+     */
+    public Triplet<V, E, M> parseGraphFileLine(String line);
+
 }
 
 
